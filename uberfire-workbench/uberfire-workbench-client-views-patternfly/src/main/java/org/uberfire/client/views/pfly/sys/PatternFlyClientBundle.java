ceb83b226784b6f2456fbe6237492cf5e330aae8
Tiago Bento <1584568+tiagobento@users.noreply.github.com>
2021-05-17 10:07:53 -0300

    Remove unused import

--- uberfire-workbench/uberfire-workbench-client-views-patternfly/src/main/java/org/uberfire/client/views/pfly/sys/PatternFlyClientBundle.java
+++ uberfire-workbench/uberfire-workbench-client-views-patternfly/src/main/java/org/uberfire/client/views/pfly/sys/PatternFlyClientBundle.java
@@ -18,7 +18,6 @@ package org.uberfire.client.views.pfly.sys;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
-import com.google.gwt.resources.client.DataResource;
 import com.google.gwt.resources.client.TextResource;
 
 public interface PatternFlyClientBundle extends ClientBundle {
