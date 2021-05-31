e61d42c74ed23e54bb1c997b329fb7982e0178ff
Yeser Amer <yamer@redhat.com>
2021-05-31 13:55:52 +0200

    KOGITO-5144: Create component structure based on the new boxed expression component (#6)

---
+++ kie-wb-common-react/import-java-classes/showcase/src/index.tsx
@@ -0,0 +1,9 @@
+import "@patternfly/react-core/dist/styles/base.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(
+  <></>,
+  document.getElementById('root')
+);
