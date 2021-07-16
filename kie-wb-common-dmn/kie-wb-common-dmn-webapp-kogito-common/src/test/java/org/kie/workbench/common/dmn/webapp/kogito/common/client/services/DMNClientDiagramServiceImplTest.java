70d662b392fba2422f03753dd7289b6e539e36e7
Tiago Bento <tiagobentofernandes@gmail.com>
2021-07-16 14:57:31 -0300

    Remove 'root' from Metadata

--- kie-wb-common-dmn/kie-wb-common-dmn-webapp-kogito-common/src/test/java/org/kie/workbench/common/dmn/webapp/kogito/common/client/services/DMNClientDiagramServiceImplTest.java
+++ kie-wb-common-dmn/kie-wb-common-dmn-webapp-kogito-common/src/test/java/org/kie/workbench/common/dmn/webapp/kogito/common/client/services/DMNClientDiagramServiceImplTest.java
@@ -138,8 +138,7 @@ public class DMNClientDiagramServiceImplTest {
 
         final Metadata metadata = service.buildMetadataInstance(fileName);
 
-        assertEquals("default://main@system/stunner/diagrams", metadata.getRoot().toURI());
-        assertEquals("default://main@system/stunner/diagrams/file.dmn", metadata.getPath().toURI());
+        assertEquals("/file.dmn", metadata.getPath().toURI());
     }
 
     @Test
