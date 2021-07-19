6e293f7762f418641645e0b540a489385d7ba449
Yeser Amer <yamer@redhat.com>
2021-07-19 09:16:36 +0200

    KOGITO-5147: Data Types - Wizard - Create the first step of the Wizard (#16)
    KOGITO-5147: Enabling DataList in First Step Wizard

--- kie-wb-common-react/import-java-classes/showcase/src/index.tsx
+++ kie-wb-common-react/import-java-classes/showcase/src/index.tsx
@@ -26,16 +26,49 @@ const Showcase: React.FunctionComponent = () => {
   const [buttonTooltipMessage, setButtonTooltipMessage] = useState(LSP_SERVER_NOT_AVAILABLE);
   const onSelectChange = useCallback((event) => setButtonDisableStatus(event.target.value === "true"), []);
   const onInputChange = useCallback((event) => setButtonTooltipMessage(event.target.value), []);
+  /* This function temporary mocks a call to the LSP service method getClasses */
+  const lspGetClassServiceMocked = (value: string) => {
+    /* Mocked data retrieved from LSP Service */
+    const booClassesList = ["org.kie.test.kogito.Book", "org.kie.test.kogito.Boom"];
+    const bookClassesList = ["org.kie.test.kogito.Book"];
+    const boomClassesList = ["org.kie.test.kogito.Boom"];
+
+    /* Temporary mocks managing */
+    if (value === "Boo") {
+      return booClassesList;
+    } else if (value === "Book") {
+      return bookClassesList;
+    } else if (value === "Boom") {
+      return boomClassesList;
+    } else {
+      return [];
+    }
+  };
+
+  window.envelopeMock = {
+    lspGetClassServiceMocked: (value: string) => lspGetClassServiceMocked(value),
+  };
 
   return (
     <div className="showcase">
+      <p>
+        This showcase demonstrates how the <strong>Import Java Classes</strong> component works. Adding the component to
+        the DOM, will result to render a Button with <em>Import Java Classes</em> label. As default status, the button
+        is disabled with a tooltip reporting the reason. Using the above menu, you can modify the button status and the
+        related tooltip message.
+      </p>
+      <p>
+        To simulate the searching of a Java Classes on the Search box inside the wizard, please use values:
+        <em>Boo</em>, <em>Boom</em> or <em>Book</em> as key, which are mocked in this showcase to demonstrate the
+        component usage.
+      </p>
       <div className="menu">
-        <h1>Import Java classes button state</h1>
+        <strong>Import Java classes button state</strong>
         <select onChange={onSelectChange}>
           <option value="true">Disabled</option>
           <option value="false">Enabled</option>
         </select>
-        <h1>Tooltip Message (Optional)</h1>
+        <strong>Tooltip Message (Optional)</strong>
         <input value={buttonTooltipMessage} onChange={onInputChange} />
       </div>
       <div className="import-java-classes">
