67b8abbf49761540d688e73d1ba634e0e94203e6
vpellegrino <vpellegr@redhat.com>
2021-08-12 15:27:52 +0200

    Service for the JSNI with BoxedExpression world

---
+++ kie-wb-common-dmn/kie-wb-common-dmn-client/src/main/java/org/kie/workbench/common/dmn/client/editors/expressions/jsinterop/util/BoxedExpressionService.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2021 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.kie.workbench.common.dmn.client.editors.expressions.jsinterop.util;
+
+import org.kie.workbench.common.dmn.client.editors.expressions.ExpressionEditorViewImpl;
+
+public class BoxedExpressionService {
+    public static void registerBroadcastForExpression(final ExpressionEditorViewImpl boxedExpressionEditorPanel) {
+        createNamespace();
+        registerResetExpressionDefinition(boxedExpressionEditorPanel);
+        registerBroadcastForLiteralExpression(boxedExpressionEditorPanel);
+        registerBroadcastForContextExpression(boxedExpressionEditorPanel);
+        registerBroadcastForRelationExpression(boxedExpressionEditorPanel);
+    }
+
+    private static native void createNamespace()/*-{
+        $wnd["beeApi"] = {};
+    }-*/;
+
+    private static native void registerResetExpressionDefinition(final ExpressionEditorViewImpl boxedExpressionEditorPanel)/*-{
+        $wnd["beeApi"].resetExpressionDefinition = function() {
+            return boxedExpressionEditorPanel.@ExpressionEditorViewImpl::resetExpressionDefinition(*)();
+        };
+    }-*/;
+
+    private static native void registerBroadcastForLiteralExpression(final ExpressionEditorViewImpl boxedExpressionEditorPanel)/*-{
+        $wnd["beeApi"].broadcastLiteralExpressionDefinition = function(literalExpressionDefinition) {
+            return boxedExpressionEditorPanel.@ExpressionEditorViewImpl::broadcastLiteralExpressionDefinition(*)(literalExpressionDefinition);
+        };
+    }-*/;
+
+    private static native void registerBroadcastForContextExpression(final ExpressionEditorViewImpl boxedExpressionEditorPanel)/*-{
+        $wnd["beeApi"].broadcastContextExpressionDefinition = function(contextExpressionDefinition) {
+            return boxedExpressionEditorPanel.@ExpressionEditorViewImpl::broadcastContextExpressionDefinition(*)(contextExpressionDefinition);
+        };
+    }-*/;
+
+    private static native void registerBroadcastForRelationExpression(final ExpressionEditorViewImpl boxedExpressionEditorPanel)/*-{
+        $wnd["beeApi"].broadcastRelationExpressionDefinition = function(relationExpressionDefinition) {
+            return boxedExpressionEditorPanel.@ExpressionEditorViewImpl::broadcastRelationExpressionDefinition(*)(relationExpressionDefinition);
+        };
+    }-*/;
+}
