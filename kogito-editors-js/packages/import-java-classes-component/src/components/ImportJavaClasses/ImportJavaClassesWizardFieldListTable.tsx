dd437bddb6b67d04cf137b3c4061eccae0cec99c
yesamer <yamer@redhat.com>
2021-08-30 12:15:59 +0200

    KOGITO-5149: Minor changes

--- kogito-editors-js/packages/import-java-classes-component/src/components/ImportJavaClasses/ImportJavaClassesWizardFieldListTable.tsx
+++ kogito-editors-js/packages/import-java-classes-component/src/components/ImportJavaClasses/ImportJavaClassesWizardFieldListTable.tsx
@@ -54,10 +54,10 @@ export const ImportJavaClassesWizardFieldListTable: React.FunctionComponent<Impo
                 expand={
                   javaClass.fields && javaClass.fields.length > 0
                     ? {
-                      rowIndex: index,
-                      isExpanded: expanded[index],
-                      onToggle: handleExpansionToggle,
-                    }
+                        rowIndex: index,
+                        isExpanded: expanded[index],
+                        onToggle: handleExpansionToggle,
+                      }
                     : undefined
                 }
               />
@@ -72,19 +72,19 @@ export const ImportJavaClassesWizardFieldListTable: React.FunctionComponent<Impo
           const childRow =
             javaClass.fields && javaClass.fields.length > 0
               ? javaClass.fields.map((field) => {
-                rowIndex += 1;
-                return (
-                  <Tr key={rowIndex} isExpanded={expanded[index] === true}>
-                    <Td key={`${rowIndex}_0`} />
-                    <Td key={`${rowIndex}_${field.name}`}>
-                      <ExpandableRowContent>
-                        <span>{field.name}</span>
-                        <span>{decorateWithRoundBrackets(field.type)}</span>
-                      </ExpandableRowContent>
-                    </Td>
-                  </Tr>
-                );
-              })
+                  rowIndex += 1;
+                  return (
+                    <Tr key={rowIndex} isExpanded={expanded[index] === true}>
+                      <Td key={`${rowIndex}_0`} />
+                      <Td key={`${rowIndex}_${field.name}`}>
+                        <ExpandableRowContent>
+                          <span>{field.name}</span>
+                          <span>{decorateWithRoundBrackets(field.type)}</span>
+                        </ExpandableRowContent>
+                      </Td>
+                    </Tr>
+                  );
+                })
               : undefined;
           return (
             <Tbody key={index} isExpanded={expanded[index] === true}>
