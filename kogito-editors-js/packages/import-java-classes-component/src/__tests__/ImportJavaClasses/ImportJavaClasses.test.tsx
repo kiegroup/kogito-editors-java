5a76e8c3746850acd253f675eed464e86181bd49
yesamer <yamer@redhat.com>
2021-08-30 14:04:39 +0200

    KOGITO-5149: Tests

--- kogito-editors-js/packages/import-java-classes-component/src/__tests__/ImportJavaClasses/ImportJavaClasses.test.tsx
+++ kogito-editors-js/packages/import-java-classes-component/src/__tests__/ImportJavaClasses/ImportJavaClasses.test.tsx
@@ -18,7 +18,6 @@ import * as React from "react";
 import { fireEvent, render } from "@testing-library/react";
 import { ImportJavaClasses } from "../../components";
 import * as _ from "lodash";
-import { act } from "react-dom/test-utils";
 
 describe("ImportJavaClasses component tests", () => {
   test("should render ImportJavaClasses Button component", () => {
@@ -77,11 +76,11 @@ describe("ImportJavaClasses component tests", () => {
     checkFirstElement = baseElement.querySelector('[aria-labelledby="com.Book"]')! as HTMLInputElement;
     expect(checkFirstElement).toBeChecked();
     expect(checkSecondElement).not.toBeChecked();
-    await fireEvent.click(checkSecondElement);
+    fireEvent.click(checkSecondElement);
     checkSecondElement = baseElement.querySelector('[aria-labelledby="com.Author"]')! as HTMLInputElement;
     expect(checkFirstElement).toBeChecked();
     expect(checkSecondElement).toBeChecked();
-    await fireEvent.click(checkSecondElement);
+    fireEvent.click(checkSecondElement);
     checkSecondElement = baseElement.querySelector('[aria-labelledby="com.Author"]')! as HTMLInputElement;
     expect(checkFirstElement).toBeChecked();
     expect(checkSecondElement).not.toBeChecked();
@@ -99,9 +98,97 @@ describe("ImportJavaClasses component tests", () => {
     expect(baseElement).toMatchSnapshot();
   });
 
+  test("Should move to second step", async () => {
+    const mockedLSPGetClassService = jest.fn(value => ["com.Book", "com.Author"]);
+    lspGetClassServiceMock(mockedLSPGetClassService);
+    lspGetClassFieldServiceMock(jest.fn(lspGetClassFieldsServiceMocked));
+    const { baseElement, getByText } = render(<ImportJavaClasses buttonDisabledStatus={false}/>);
+    const modalWizardButton = getByText("Import Java classes")! as HTMLButtonElement;
+    modalWizardButton.click();
+    const inputElement = baseElement.querySelector('[aria-label="Search input"]')! as HTMLInputElement;
+    expect(inputElement).toHaveValue("");
+    expect(baseElement.querySelector('[aria-label="Reset"]')! as HTMLButtonElement).not.toBeInTheDocument();
+    fireEvent.change(inputElement, { target: { value: "test" } });
+    expect(inputElement).toHaveValue("test");
+    const firstElement = baseElement.querySelector('[id="com.Book"]')! as HTMLSpanElement;
+    expect(firstElement).toBeInTheDocument();
+    const secondElement = baseElement.querySelector('[id="com.Author"]')! as HTMLSpanElement;
+    expect(secondElement).toBeInTheDocument();
+    let checkFirstElement = baseElement.querySelector('[aria-labelledby="com.Book"]')! as HTMLInputElement;
+    expect(checkFirstElement).toBeInTheDocument();
+    expect(checkFirstElement).not.toBeChecked();
+    let checkSecondElement = baseElement.querySelector('[aria-labelledby="com.Author"]')! as HTMLInputElement;
+    expect(checkSecondElement).toBeInTheDocument();
+    expect(checkSecondElement).not.toBeChecked();
+    fireEvent.click(checkFirstElement);
+    checkFirstElement = baseElement.querySelector('[aria-labelledby="com.Book"]')! as HTMLInputElement;
+    expect(checkFirstElement).toBeChecked();
+    expect(checkSecondElement).not.toBeChecked();
+    const nextButton = getByText("Next") as HTMLButtonElement;
+    fireEvent.click(nextButton);
+    const expandToggle = baseElement.querySelector('[id="expand-toggle0"]')! as HTMLButtonElement;
+    expect(expandToggle).toHaveAttribute("aria-expanded", "true");
+    fireEvent.click(expandToggle);
+    expect(expandToggle).toHaveAttribute("aria-expanded", "false");
+    fireEvent.click(expandToggle);
+
+
+    expect(baseElement).toMatchSnapshot();
+  });
+
+  test("Should move to second step with empty fields", async () => {
+    const mockedLSPGetClassService = jest.fn(value => ["com.Book", "com.Author"]);
+    lspGetClassServiceMock(mockedLSPGetClassService);
+    lspGetClassFieldServiceMock(jest.fn(lspGetClassFieldsServiceMocked));
+    const { baseElement, getByText } = render(<ImportJavaClasses buttonDisabledStatus={false}/>);
+    const modalWizardButton = getByText("Import Java classes")! as HTMLButtonElement;
+    modalWizardButton.click();
+    const inputElement = baseElement.querySelector('[aria-label="Search input"]')! as HTMLInputElement;
+    expect(inputElement).toHaveValue("");
+    expect(baseElement.querySelector('[aria-label="Reset"]')! as HTMLButtonElement).not.toBeInTheDocument();
+    fireEvent.change(inputElement, { target: { value: "test" } });
+    expect(inputElement).toHaveValue("test");
+    const firstElement = baseElement.querySelector('[id="com.Book"]')! as HTMLSpanElement;
+    expect(firstElement).toBeInTheDocument();
+    const secondElement = baseElement.querySelector('[id="com.Author"]')! as HTMLSpanElement;
+    expect(secondElement).toBeInTheDocument();
+    let checkFirstElement = baseElement.querySelector('[aria-labelledby="com.Book"]')! as HTMLInputElement;
+    expect(checkFirstElement).toBeInTheDocument();
+    expect(checkFirstElement).not.toBeChecked();
+    let checkSecondElement = baseElement.querySelector('[aria-labelledby="com.Author"]')! as HTMLInputElement;
+    expect(checkSecondElement).toBeInTheDocument();
+    expect(checkSecondElement).not.toBeChecked();
+    fireEvent.click(checkSecondElement);
+    checkSecondElement = baseElement.querySelector('[aria-labelledby="com.Author"]')! as HTMLInputElement;
+    expect(checkFirstElement).not.toBeChecked();
+    expect(checkSecondElement).toBeChecked();
+    const nextButton = getByText("Next") as HTMLButtonElement;
+    fireEvent.click(nextButton);
+
+    expect(baseElement).toMatchSnapshot();
+  });
+
   function lspGetClassServiceMock(mockedBroadcastDefinition: jest.Mock) {
     window.envelopeMock = _.extend(window.envelopeMock || {}, {
       lspGetClassServiceMocked: (value: string) => mockedBroadcastDefinition(value),
     });
   }
+
+  function lspGetClassFieldServiceMock(mockedBroadcastDefinition: jest.Mock) {
+    window.envelopeMock = _.extend(window.envelopeMock || {}, {
+      lspGetClassFieldsServiceMocked: (className: string) => mockedBroadcastDefinition(className),
+    });
+  }
+
+  const lspGetClassFieldsServiceMocked = (className: string) => {
+    const bookClassFieldsMap = new Map<string, string>();
+    bookClassFieldsMap.set("title", "string");
+    bookClassFieldsMap.set("year", "integer");
+    if (className === "com.Book") {
+      console.log(bookClassFieldsMap);
+      return bookClassFieldsMap;
+    } else {
+      return new Map<string, string>();
+    }
+  };
 });
