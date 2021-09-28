3a29c061f3a95f75a56bd89f437eb15b147f1bb0
Guilherme Carreiro <karreiro@gmail.com>
2021-09-28 11:23:03 +0200

    Remove Monaco duplication

--- kogito-editors-js/packages/dmn-loader/src/index.tsx
+++ kogito-editors-js/packages/dmn-loader/src/index.tsx
@@ -33,6 +33,7 @@ import * as React from "react";
 import { useEffect, useState } from "react";
 import * as ReactDOM from "react-dom";
 import { setupWire } from "./wire";
+import * as Monaco from "monaco-editor";
 
 setupWire();
 
@@ -104,4 +105,4 @@ const renderHelloWorld = (selector: string) => {
   ReactDOM.render(<HelloWorld />, document.querySelector(selector));
 };
 
-export { renderHelloWorld, renderBoxedExpressionEditor };
+export { renderHelloWorld, renderBoxedExpressionEditor, Monaco };
