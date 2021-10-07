a03791e11b05d4e44662dde0f40004a066ebb621
Luiz Motta <luizjoaomotta@gmail.com>
2021-10-07 17:47:50 -0300

    Add dmn-runner-showcase

---
+++ kogito-editors-js/packages/boxed-expression-component/dmn-runner-showcase/src/unitables/core/Grid.tsx
@@ -0,0 +1,165 @@
+import { Bridge, joinName } from "uniforms";
+import * as React from "react";
+import { AutoField } from "./AutoField";
+import { DataType } from "boxed-expression-component/dist/api";
+import { DmnRunnerClause } from "../boxed";
+import { DecisionResult, Result } from "../dmn";
+
+export class Grid {
+  constructor(private readonly bridge: Bridge) {}
+
+  public getBridge() {
+    return this.bridge;
+  }
+
+  public removeInputName(fullName: string) {
+    return fullName.match(/\./) ? fullName.split(".").slice(1).join("-") : fullName;
+  }
+
+  public getDataTypeProps(type: string | undefined) {
+    let extractedType = (type ?? "").split("FEEL:").pop();
+    if ((extractedType?.length ?? 0) > 1) {
+      extractedType = (type ?? "").split(":").pop()?.split("}").join("").trim();
+    }
+    switch (extractedType) {
+      case "<Undefined>":
+        return { dataType: DataType.Undefined, width: 150 };
+      case "Any":
+        return { dataType: DataType.Any, width: 150 };
+      case "boolean":
+        return { dataType: DataType.Boolean, width: 150 };
+      case "context":
+        return { dataType: DataType.Context, width: 150 };
+      case "date":
+        return { dataType: DataType.Date, width: 180 };
+      case "date and time":
+        return { dataType: DataType.DateTime, width: 282 };
+      case "days and time duration":
+        return { dataType: DataType.DateTimeDuration, width: 150 };
+      case "number":
+        return { dataType: DataType.Number, width: 150 };
+      case "string":
+        return { dataType: DataType.String, width: 150 };
+      case "time":
+        return { dataType: DataType.Time, width: 150 };
+      case "years and months duration":
+        return { dataType: DataType.YearsMonthsDuration, width: 150 };
+      default:
+        return { dataType: (extractedType as DataType) ?? DataType.Undefined, width: undefined };
+    }
+  }
+
+  public deepGenerateBoxed(fieldName: any, parentName = ""): any {
+    const joinedName = joinName(parentName, fieldName);
+    const field = this.bridge.getField(joinedName);
+
+    if (field.type === "object") {
+      const insideProperties = this.bridge.getSubfields(joinedName).reduce((acc: any[], subField: string) => {
+        const field = this.deepGenerateBoxed(subField, joinedName);
+        if (field.insideProperties) {
+          return [...acc, ...field.insideProperties];
+        }
+        return [...acc, field];
+      }, []);
+      return {
+        ...this.getDataTypeProps(field["x-dmn-type"]),
+        insideProperties,
+        name: joinedName,
+      };
+    }
+    return {
+      ...this.getDataTypeProps(field["x-dmn-type"]),
+      name: this.removeInputName(joinedName),
+      cellDelegate: ({ formId }: any) => <AutoField key={joinedName} name={joinedName} form={formId} />,
+    };
+  }
+
+  public generateBoxedInputs(): DmnRunnerClause[] {
+    let myGrid: DmnRunnerClause[] = [];
+    const subfields = this.bridge.getSubfields();
+    const inputs = subfields.reduce(
+      (acc: DmnRunnerClause[], fieldName: string) => [...acc, this.deepGenerateBoxed(fieldName)],
+      [] as DmnRunnerClause[]
+    );
+    if (inputs.length > 0) {
+      myGrid = inputs;
+    }
+    return myGrid;
+  }
+
+  private deepFlattenOutput(acc: any, entry: string, value: object) {
+    return Object.entries(value).map(([deepEntry, deepValue]) => {
+      if (typeof deepValue === "object" && deepValue !== null) {
+        this.deepFlattenOutput(acc, deepEntry, deepValue);
+      }
+      acc[`${entry}-${deepEntry}`] = deepValue;
+      return acc;
+    });
+  }
+
+  public generateBoxedOutputs(
+    schema: any,
+    decisionResults: Array<DecisionResult[] | undefined>
+  ): [Map<string, DmnRunnerClause>, Result[]] {
+    const outputTypeMap = Object.entries(schema?.definitions?.OutputSet?.properties ?? []).reduce(
+      (acc: Map<string, DataType>, [name, properties]: [string, any]) => {
+        if (properties["x-dmn-type"]) {
+          acc.set(name, this.getDataTypeProps(properties["x-dmn-type"]).dataType);
+        } else {
+          const path = properties.$ref.split("/").slice(1); // remove #
+          const type = path.reduce((acc: any, property: string) => acc[property], schema);
+          acc.set(name, this.getDataTypeProps(type["x-dmn-type"]).dataType);
+        }
+
+        return acc;
+      },
+      new Map<string, DataType>()
+    );
+
+    const outputSet = decisionResults.reduce(
+      (acc: Map<string, DmnRunnerClause>, decisionResult: DecisionResult[] | undefined) => {
+        if (decisionResult) {
+          decisionResult.forEach(({ decisionName }) => {
+            acc.set(decisionName, {
+              name: decisionName,
+              dataType: outputTypeMap.get(decisionName) ?? DataType.Undefined,
+            });
+          });
+        }
+        return acc;
+      },
+      new Map<string, DmnRunnerClause>()
+    );
+
+    const outputEntries = decisionResults.reduce((acc: Result[], decisionResult: DecisionResult[] | undefined) => {
+      if (decisionResult) {
+        const outputResults = decisionResult.map(({ result }) => {
+          if (result === null) {
+            return "null";
+          }
+          if (result === true) {
+            return "true";
+          }
+          if (result === false) {
+            return "false";
+          }
+          if (typeof result === "object") {
+            return Object.entries(result).reduce((acc: any, [entry, value]) => {
+              if (typeof value === "object" && value !== null) {
+                this.deepFlattenOutput(acc, entry, value);
+              } else {
+                acc[entry] = value;
+              }
+              return acc;
+            }, {});
+          }
+          return result;
+        });
+        return [...acc, outputResults];
+      }
+      return acc;
+    }, []);
+
+    return [outputSet, outputEntries];
+  }
+}
---
+++ kogito-editors-js/packages/boxed-expression-component/dmn-runner-showcase/src/unitables/dmn/DmnAutoTable.tsx
@@ -0,0 +1,257 @@
+import * as React from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { Clause, TableOperation } from "boxed-expression-component/dist/api";
+import { DmnValidator } from "./DmnValidator";
+import { AutoRow } from "../core";
+import { createPortal } from "react-dom";
+import { context as UniformsContext } from "uniforms";
+import { ErrorBoundary } from "../common/ErrorBoundary";
+import { EmptyState, EmptyStateBody, EmptyStateIcon } from "@patternfly/react-core/dist/js/components/EmptyState";
+import { ExclamationIcon } from "@patternfly/react-icons/dist/js/icons/exclamation-icon";
+import { Text, TextContent } from "@patternfly/react-core/dist/js/components/Text";
+import { DmnGrid } from "./DmnGrid";
+import { DmnRunnerRule, DmnRunnerTableBoxed, DmnRunnerTableProps } from "../boxed";
+import { NotificationSeverity } from "@kogito-tooling/notifications/dist/api";
+import { dmnAutoTableDictionaries, DmnAutoTableI18nContext, dmnAutoTableI18nDefaults } from "../i18n";
+import { I18nDictionariesProvider } from "@kogito-tooling/i18n/dist/react-components";
+import nextId from "react-id-generator";
+import { BoxedExpressionProvider } from "boxed-expression-component/dist/components";
+
+export enum EvaluationStatus {
+  SUCCEEDED = "SUCCEEDED",
+  SKIPPED = "SKIPPED",
+  FAILED = "FAILED",
+}
+
+export interface DecisionResultMessage {
+  severity: NotificationSeverity;
+  message: string;
+  messageType: string;
+  sourceId: string;
+  level: string;
+}
+
+export type Result = boolean | number | null | object | object[] | string;
+
+export interface DecisionResult {
+  decisionId: string;
+  decisionName: string;
+  result: Result;
+  messages: DecisionResultMessage[];
+  evaluationStatus: EvaluationStatus;
+}
+
+export interface DmnResult {
+  details?: string;
+  stack?: string;
+  decisionResults?: DecisionResult[];
+  messages: DecisionResultMessage[];
+}
+
+interface Props {
+  schema: any;
+  tableData?: any;
+  setTableData?: React.Dispatch<React.SetStateAction<any>>;
+  results?: Array<DecisionResult[] | undefined>;
+  formError: boolean;
+  setFormError: React.Dispatch<any>;
+}
+
+const FORMS_ID = "unitable-forms";
+
+export function DmnAutoTable(props: Props) {
+  const errorBoundaryRef = useRef<ErrorBoundary>(null);
+
+  const [rowQuantity, setRowQuantity] = useState<number>(1);
+  const [formsDivRendered, setFormsDivRendered] = useState<boolean>(false);
+
+  const bridge = useMemo(() => new DmnValidator().getBridge(props.schema ?? {}), [props.schema]);
+  const grid = useMemo(() => (bridge ? new DmnGrid(bridge) : undefined), [bridge]);
+  const shouldRender = useMemo(() => (grid?.generateBoxedInputs().length ?? 0) > 0, [grid]);
+
+  const handleOperation = useCallback(
+    (tableOperation: TableOperation, rowIndex: number) => {
+      switch (tableOperation) {
+        case TableOperation.RowInsertAbove:
+          props.setTableData?.((previousTableData: any) => {
+            return [...previousTableData.slice(0, rowIndex), {}, ...previousTableData.slice(rowIndex)];
+          });
+          break;
+        case TableOperation.RowInsertBelow:
+          props.setTableData?.((previousTableData: any) => {
+            return [...previousTableData.slice(0, rowIndex + 1), {}, ...previousTableData.slice(rowIndex + 1)];
+          });
+          break;
+        case TableOperation.RowDelete:
+          props.setTableData?.((previousTableData: any) => {
+            return [...previousTableData.slice(0, rowIndex), ...previousTableData.slice(rowIndex + 1)];
+          });
+          break;
+        case TableOperation.RowClear:
+          props.setTableData?.((previousTableData: any) => {
+            const newTableData = [...previousTableData];
+            newTableData[rowIndex] = {};
+            return newTableData;
+          });
+          break;
+        case TableOperation.RowDuplicate:
+          props.setTableData?.((previousTableData: any) => {
+            return [
+              ...previousTableData.slice(0, rowIndex + 1),
+              previousTableData[rowIndex],
+              ...previousTableData.slice(rowIndex + 1),
+            ];
+          });
+      }
+    },
+    [props.setTableData]
+  );
+
+  const onRowNumberUpdated = useCallback(
+    (rowQtt: number, operation?: TableOperation, rowIndex?: number) => {
+      setRowQuantity(rowQtt);
+      if (operation !== undefined && rowIndex !== undefined) {
+        handleOperation(operation, rowIndex);
+      }
+    },
+    [handleOperation]
+  );
+
+  const onSubmit = useCallback(
+    (model: any, index) => {
+      props.setTableData?.((previousTableData: any) => {
+        const newTableData = [...previousTableData];
+        newTableData[index] = model;
+        return newTableData;
+      });
+    },
+    [props.setTableData]
+  );
+
+  const onValidate = useCallback(
+    (model: any, error: any, index) => {
+      props.setTableData?.((previousTableData: any) => {
+        const newTableData = [...previousTableData];
+        newTableData[index] = model;
+        return newTableData;
+      });
+    },
+    [props.setTableData]
+  );
+
+  const getAutoRow = useCallback(
+    (data, rowIndex: number) =>
+      ({ children }: any) =>
+        (
+          <AutoRow
+            schema={bridge}
+            autosave={true}
+            autosaveDelay={500}
+            model={data}
+            onSubmit={(model: any) => onSubmit(model, rowIndex)}
+            onValidate={(model: any, error: any) => onValidate(model, error, rowIndex)}
+            placeholder={true}
+          >
+            <UniformsContext.Consumer>
+              {(ctx: any) => (
+                <>
+                  {createPortal(
+                    <form id={`dmn-auto-form-${rowIndex}`} onSubmit={(data) => ctx?.onSubmit(data)} />,
+                    document.getElementById(FORMS_ID)!
+                  )}
+                  {children}
+                </>
+              )}
+            </UniformsContext.Consumer>
+          </AutoRow>
+        ),
+    [bridge, onSubmit, onValidate]
+  );
+
+  let selectedExpression: DmnRunnerTableProps | undefined = undefined;
+  selectedExpression = useMemo(() => {
+    const filteredResults = props.results?.filter((result) => result !== undefined);
+    if (grid && filteredResults) {
+      const input = grid.generateBoxedInputs();
+      const [outputSet, outputEntries] = grid.generateBoxedOutputs(props.schema ?? {}, filteredResults);
+      const output: Clause[] = Array.from(outputSet.values());
+
+      const rules = [];
+      const inputEntriesLength = input.reduce(
+        (acc, i) => (i.insideProperties ? acc + i.insideProperties.length : acc + 1),
+        0
+      );
+      const inputEntries = new Array(inputEntriesLength);
+      for (let i = 0; i < rowQuantity; i++) {
+        const rule: DmnRunnerRule = {
+          inputEntries,
+          outputEntries: (outputEntries?.[i] as string[]) ?? [],
+        };
+        if (formsDivRendered) {
+          rule.rowDelegate = getAutoRow(props.tableData[i], i);
+        }
+        rules.push(rule);
+      }
+
+      return {
+        name: "DMN Runner",
+        input,
+        output,
+        rules,
+        uid: selectedExpression?.uid ?? nextId(),
+        onRowNumberUpdated,
+      };
+    }
+  }, [
+    grid,
+    props.schema,
+    props.results,
+    props.tableData,
+    rowQuantity,
+    onRowNumberUpdated,
+    formsDivRendered,
+    getAutoRow,
+  ]);
+
+  const formErrorMessage = useMemo(
+    () => (
+      <div>
+        <EmptyState>
+          <EmptyStateIcon icon={ExclamationIcon} />
+          <TextContent>
+            <Text component={"h2"}>Error</Text>
+          </TextContent>
+          <EmptyStateBody>
+            <p>An error has happened</p>
+          </EmptyStateBody>
+        </EmptyState>
+      </div>
+    ),
+    []
+  );
+
+  // Resets the ErrorBoundary everytime the FormSchema is updated
+  useEffect(() => {
+    errorBoundaryRef.current?.reset();
+  }, [bridge]);
+
+  return (
+    <>
+      {shouldRender && bridge && selectedExpression && (
+        <ErrorBoundary ref={errorBoundaryRef} setHasError={props.setFormError} error={formErrorMessage}>
+          <I18nDictionariesProvider
+            defaults={dmnAutoTableI18nDefaults}
+            dictionaries={dmnAutoTableDictionaries}
+            initialLocale={navigator.language}
+            ctx={DmnAutoTableI18nContext}
+          >
+            <BoxedExpressionProvider expressionDefinition={selectedExpression} isRunnerTable={true}>
+              <DmnRunnerTableBoxed {...selectedExpression} />
+            </BoxedExpressionProvider>
+          </I18nDictionariesProvider>
+        </ErrorBoundary>
+      )}
+      <div ref={() => setFormsDivRendered(true)} id={FORMS_ID} />
+    </>
+  );
+}
